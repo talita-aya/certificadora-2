dde5a976a72d390fa083e78351b91f03b4e91023
Talita Aya <tatakuriki@gmail.com>
2023-10-15 16:20:01 -0300

    linha com representação do ângulo

--- projetil.js
+++ projetil.js
@@ -26,13 +26,24 @@ function calcularLancamentoProjetil() {
     // limpar o canvas para recomeçar
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    // Desenhar a reta inclinada
+    ctx.strokeStyle = "orange";
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.moveTo(0, canvas.height);
+    const deltaX = Math.cos(angleRad) * canvas.width;
+    const deltaY = -Math.sin(angleRad) * canvas.width;
+    ctx.lineTo(deltaX, canvas.height + deltaY);
+    ctx.stroke();
+    ctx.closePath();
+
     // linha pontilhada para trajetória
     ctx.setLineDash([3, 3]);
 
     // trajetória do lançamento de projétil
     ctx.beginPath();
     ctx.moveTo(0, canvas.height);
-    let lastX, lastY; 
+    let lastX, lastY;
     for (let t = 0; t <= time; t += 0.1) {
         const x = velocity * Math.cos(angleRad) * t;
         const y =
@@ -40,20 +51,31 @@ function calcularLancamentoProjetil() {
             (velocity * Math.sin(angleRad) * t - 0.5 * 9.81 * Math.pow(t, 2));
         ctx.lineTo(x, y);
         lastX = x;
-        lastY = y; 
+        lastY = y;
     }
 
     ctx.strokeStyle = "#FFE0B1";
     ctx.lineWidth = 2;
-    ctx.closePath();
     ctx.stroke();
 
     // bolinha no final
     ctx.beginPath();
-    ctx.arc(lastX-8, lastY-5, 8, 0, Math.PI * 2);
+    ctx.arc(lastX, lastY, 8, 0, Math.PI * 2);
     ctx.fillStyle = "#31C6F6";
     ctx.fill();
-    ctx.closePath();
+
+    // mostrar o ângulo
+    ctx.fillStyle = "orange";
+    ctx.font = "12px Arial";
+    ctx.fillText(`${angle.toFixed(0)}°`, 40, canvas.height - 10);
+
+    // Desenhar o arco interno para representar o ângulo em relação ao eixo x
+    ctx.strokeStyle = "orange";
+    ctx.lineWidth = 2;
+    ctx.setLineDash([]);
+    ctx.beginPath();
+    ctx.arc(0, canvas.height, 30, 0, -angleRad, true);
+    ctx.stroke();
 
     ctx.setLineDash([]);
 }
