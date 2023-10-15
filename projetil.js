ed1b755ad2ae0953d81eaaad68cbe1867a5a9282
Talita Aya <tatakuriki@gmail.com>
2023-10-15 15:53:14 -0300

    novo gráfico de coordenadas x e y

--- projetil.js
+++ projetil.js
@@ -1,113 +1,63 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const angleInput = document.getElementById("angle");
-    const velocityInput = document.getElementById("velocity");
-    const buttonSimulator = document.getElementById("button-simulator");
-    const rangeResult = document.getElementById("range");
-    const heightResult = document.getElementById("height");
-    const timeResult = document.getElementById("time");
+// Função para calcular o lançamento de projétil
+function calcularLancamentoProjetil() {
+    const velocity = parseFloat(document.getElementById("velocity").value);
+    const angle = parseFloat(document.getElementById("angle").value);
     const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
 
-    buttonSimulator.addEventListener("click", function () {
-        const angle = parseFloat(angleInput.value);
-        const velocity = parseFloat(velocityInput.value);
+    // ângulo de graus para radianos
+    const angleRad = (angle * Math.PI) / 180;
 
-        if (!isNaN(angle) && !isNaN(velocity)) {
-            const radians = (angle * Math.PI) / 180;
-            const g = 9.81;
-            const dataPoints = [];
-            const timeStep = 0.1;
-            let t = 0;
+    // altura máxima
+    const height =
+        (Math.pow(velocity, 2) * Math.pow(Math.sin(angleRad), 2)) / (2 * 9.81);
 
-            while (t <= (2 * velocity * Math.sin(radians)) / g) {
-                const x = velocity * Math.cos(radians) * t;
-                const y = velocity * Math.sin(radians) * t - (0.5 * g * t * t);
-                dataPoints.push({ time: t, height: y });
-                t += timeStep;
-            }
+    // distância
+    const range = (Math.pow(velocity, 2) * Math.sin(2 * angleRad)) / 9.81;
 
-            rangeResult.textContent = (dataPoints[dataPoints.length - 1].time * velocity * Math.cos(radians)).toFixed(2);
-            heightResult.textContent = (dataPoints.reduce((max, point) => (point.height > max ? point.height : max), -Infinity)).toFixed(2);
-            timeResult.textContent = dataPoints[dataPoints.length - 1].time.toFixed(2);
+    // tempo
+    const time = (2 * velocity * Math.sin(angleRad)) / 9.81;
 
-            updateTrajectoryChart(dataPoints, radians);
-        } else {
-            alert("Os dados inseridos são inválidos, tente novamente");
-        }
-    });
+    // resultados
+    document.getElementById("height").textContent = height.toFixed(2);
+    document.getElementById("range").textContent = range.toFixed(2);
+    document.getElementById("time").textContent = time.toFixed(2);
 
-    function updateTrajectoryChart(dataPoints, angleRadians) {
-        const labels = dataPoints.map(point => point.time.toFixed(2));
-        const heights = dataPoints.map(point => point.height.toFixed(2));
+    // limpar o canvas para recomeçar
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        // Calcula os pontos da linha vermelha com base no ângulo
-        const angleData = [];
-        for (let i = 0; i < dataPoints.length; i++) {
-            const x = dataPoints[i].time;
-            const y = x * Math.tan(angleRadians);
-            angleData.push(y);
-        }
+    // linha pontilhada para trajetória
+    ctx.setLineDash([3, 3]);
 
-        const ctx = document.getElementById('canvas').getContext('2d');
-        const chart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: labels,
-                datasets: [
-                    {
-                        label: 'Percurso do Projétil',
-                        data: heights,
-                        borderColor: 'blue',
-                        borderWidth: 1,
-                        fill: false,
-                    },
-                    {
-                        label: 'Ângulo',
-                        data: angleData,
-                        borderColor: 'orange',
-                        borderWidth: 1,
-                        fill: false,
-                        borderDash: [5, 5]
-                    }
-                ]
-            },
-            options: {
-                scales: {
-                    x: {
-                        type: 'linear',
-                        position: 'bottom',
-                        title: {
-                            display: true,
-                            text: 'Tempo (s)'
-                        },
-                        grid: { // Configuração das grades no eixo X
-                            display: false, // Desativa as grades no eixo X
-                        }
-                    },
-                    y: {
-                        type: 'linear',
-                        position: 'left',
-                        title: {
-                            display: true,
-                            text: 'Altura (m)'
-                        },
-                        grid: { // Configuração das grades no eixo Y
-                            display: false, // Desativa as grades no eixo Y
-                        }
-                    }
-                }
-            }
-        });
-
-        // Atualiza a posição do ângulo no gráfico
-        chart.data.datasets[1].data = angleData;
-        chart.update();
-
-        // Calcula a posição do ângulo no gráfico
-        const anglePosition = {
-            x: 0.1 * dataPoints[dataPoints.length - 1].time,
-            y: 0.1 * Math.tan(angleRadians) * dataPoints[dataPoints.length - 1].time
-        };
-        angleInChartInput.value = `X: ${anglePosition.x.toFixed(2)}, Y: ${anglePosition.y.toFixed(2)}`;
+    // trajetória do lançamento de projétil
+    ctx.beginPath();
+    ctx.moveTo(0, canvas.height);
+    let lastX, lastY; 
+    for (let t = 0; t <= time; t += 0.1) {
+        const x = velocity * Math.cos(angleRad) * t;
+        const y =
+            canvas.height -
+            (velocity * Math.sin(angleRad) * t - 0.5 * 9.81 * Math.pow(t, 2));
+        ctx.lineTo(x, y);
+        lastX = x;
+        lastY = y; 
     }
 
-});
+    ctx.strokeStyle = "#FFE0B1";
+    ctx.lineWidth = 2;
+    ctx.closePath();
+    ctx.stroke();
+
+    // bolinha no final
+    ctx.beginPath();
+    ctx.arc(lastX-8, lastY-5, 8, 0, Math.PI * 2);
+    ctx.fillStyle = "#31C6F6";
+    ctx.fill();
+    ctx.closePath();
+
+    ctx.setLineDash([]);
+}
+
+document
+    .getElementById("button-simulator")
+    .addEventListener("click", calcularLancamentoProjetil);
