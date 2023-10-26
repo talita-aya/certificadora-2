82184a33ae9fe4fad580b788909fb5fba9364ccf
Talita Aya <tatakuriki@gmail.com>
2023-10-26 14:19:40 -0300

    estrutura de dados adicionada

--- projetil.js
+++ projetil.js
@@ -9,22 +9,28 @@ function calcularLancamentoProjetil() {
         return;
     }
 
-    // ângulo de graus para radianos
-    const angleRad = (angle * Math.PI) / 180;
+    const projectile = {
+        velocity: velocity,
+        angle: angle,
+        angleRad: (angle * Math.PI) / 180,
+        height: 0,
+        range: 0,
+        time: 0,
+    }
 
     // altura máxima
-    const height = (Math.pow(velocity, 2) * Math.pow(Math.sin(angleRad), 2)) / (2 * 9.81);
+    projectile.height = (Math.pow(projectile.velocity, 2) * Math.pow(Math.sin(projectile.angleRad), 2)) / (2 * 9.81);
 
     // distância
-    const range = (Math.pow(velocity, 2) * Math.sin(2 * angleRad)) / 9.81;
+    projectile.range = (Math.pow(projectile.velocity, 2) * Math.sin(2 * projectile.angleRad)) / 9.81;
 
     // tempo
-    const time = (2 * velocity * Math.sin(angleRad)) / 9.81;
+    projectile.time = (2 * projectile.velocity * Math.sin(projectile.angleRad)) / 9.81;
 
     // resultados
-    document.getElementById("height").textContent = height.toFixed(2);
-    document.getElementById("range").textContent = range.toFixed(2);
-    document.getElementById("time").textContent = time.toFixed(2);
+    document.getElementById("height").textContent = projectile.height.toFixed(2);
+    document.getElementById("range").textContent = projectile.range.toFixed(2);
+    document.getElementById("time").textContent = projectile.time.toFixed(2);
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -33,8 +39,8 @@ function calcularLancamentoProjetil() {
     ctx.lineWidth = 2;
     ctx.beginPath();
     ctx.moveTo(0, canvas.height);
-    const deltaX = Math.cos(angleRad) * canvas.width;
-    const deltaY = -Math.sin(angleRad) * canvas.width;
+    const deltaX = Math.cos(projectile.angleRad) * canvas.width;
+    const deltaY = -Math.sin(projectile.angleRad) * canvas.width;
     ctx.lineTo(deltaX, canvas.height + deltaY);
     ctx.stroke();
     ctx.closePath();
@@ -46,11 +52,11 @@ function calcularLancamentoProjetil() {
     ctx.beginPath();
     ctx.moveTo(0, canvas.height);
     let lastX, lastY;
-    for (let t = 0; t <= time; t += 0.1) {
-        const x = velocity * Math.cos(angleRad) * t;
+    for (let t = 0; t <= projectile.time; t += 0.1) {
+        const x = projectile.velocity * Math.cos(projectile.angleRad) * t;
         const y =
             canvas.height -
-            (velocity * Math.sin(angleRad) * t - 0.5 * 9.81 * Math.pow(t, 2));
+            (projectile.velocity * Math.sin(projectile.angleRad) * t - 0.5 * 9.81 * Math.pow(t, 2));
         ctx.lineTo(x, y);
         lastX = x;
         lastY = y;
@@ -68,13 +74,13 @@ function calcularLancamentoProjetil() {
     // mostrar o ângulo
     ctx.fillStyle = "orange";
     ctx.font = "12px Arial";
-    ctx.fillText(`${angle.toFixed(0)}°`, 40, canvas.height - 10);
+    ctx.fillText(`${projectile.angle.toFixed(0)}°`, 40, canvas.height - 10);
 
     ctx.strokeStyle = "orange";
     ctx.lineWidth = 2;
     ctx.setLineDash([]);
     ctx.beginPath();
-    ctx.arc(0, canvas.height, 30, 0, -angleRad, true);
+    ctx.arc(0, canvas.height, 30, 0, -projectile.angleRad, true);
     ctx.stroke();
 
     ctx.setLineDash([]);
